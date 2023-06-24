cfc08ff11ce012d3748a1f2955592f096c6085de
amesky <loup.quehec@gmail.com>
2023-06-24 13:30:46 +0200

    init

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Paradise Pictures",
+  description:
+    "BEautiful web movie app !",
+  mainNav: [
+    {
+      title: "Accueil",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/xmesky",
+    github: "https://github.com/xmesky",
+  },
+}
