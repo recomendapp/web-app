d871d41fe3337a3f89b68c986e20b7162f79a479
amesky <loup.quehec@gmail.com>
2023-10-24 20:47:43 +0200

    fix navigator

--- src/components/Console/HelloNerd.tsx
+++ src/components/Console/HelloNerd.tsx
@@ -5,18 +5,9 @@ export default function HelloNerd() {
         console.log(`\n%cRECOMEND ${v} 🎥`, 'color:#FFE974; background:#000; font-size:1.5rem; padding:0.15rem 0.25rem; margin: 1rem auto; font-family: Rockwell; border: 2px solid #FFE974; border-radius: 4px;font-weight: bold; text-shadow: 1px 1px 1px #948951;');
         
     }
-    function tables() {
-        console.table({
-            'Time Stamp': new Date().getTime(),
-            'OS': navigator['platform'],
-            'Browser': navigator['appCodeName'],
-            'Language': navigator['language'],
-        });
-    }
 
     function init() {
-        recomend();
-        tables();        
+        recomend();      
     }
 
     init();
