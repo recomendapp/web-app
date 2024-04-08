567ab58b4d062f6bd8358e03a476425f25d14c8c
loup <loup.quehec@gmail.com>
2024-04-08 18:50:08 +0200

    update stuff

--- src/app/[lang]/(app)/auth/callback/route.ts
+++ src/app/[lang]/(app)/auth/callback/route.ts
@@ -7,7 +7,7 @@ export const dynamic = 'force-dynamic';
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-  const next = requestUrl.searchParams.get('next') ?? '/';
+  const next = requestUrl.searchParams.get('next') ?? requestUrl.origin;
 
   if (code) {
     const supabase = createRouteHandlerClient();
