e2390c3dbbb39d177f1db21fc57c83c5f96a6150
loup <loup.quehec@gmail.com>
2024-04-10 10:18:11 +0200

    update stuff

--- src/app/[lang]/(app)/auth/callback/route.ts
+++ src/app/[lang]/(app)/auth/callback/route.ts
@@ -3,46 +3,47 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
+
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
-  const code = searchParams.get('code')
-  const next = searchParams.get('next') ?? '/'
-
-  const redirectTo = request.nextUrl.clone()
-  redirectTo.pathname = next
-  redirectTo.searchParams.delete('code')
-
+  const requestUrl = new URL(request.url);
   try {
-    if (!code) throw new Error('No code provided')
-      const supabase = createRouteHandlerClient()
-      const { error } = await supabase.auth.exchangeCodeForSession(code)
-      if (error) throw error
-      return NextResponse.redirect(redirectTo)
+    const code = requestUrl.searchParams.get('code');
+    const next = requestUrl.searchParams.get('next') ?? '/';
+    
+    if (!code) throw new Error('No code provided');
+    const supabase = createRouteHandlerClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (error) throw error;
+    
+    // URL to redirect to after sign in process completes
+    return NextResponse.redirect(new URL(next, requestUrl.host));
+  
   } catch (error) {
+    console.error(`error [${new Date().toISOString()}]:`, error);
     // return the user to the error page
-    const errorUrl = new URL('/auth/error', request.nextUrl)
-    errorUrl.searchParams.set('error', `${error}`)
-    return NextResponse.redirect(errorUrl)
+    return NextResponse.redirect(new URL('/auth/error', requestUrl.host));
   }
 }
 
 // export async function GET(request: NextRequest) {
-//   const requestUrl = new URL(request.url);
+//   const { searchParams } = new URL(request.url)
+//   const code = searchParams.get('code')
+//   const next = searchParams.get('next') ?? '/'
+
+//   const redirectTo = request.nextUrl.clone()
+//   redirectTo.pathname = next
+//   redirectTo.searchParams.delete('code')
+
 //   try {
-//     const code = requestUrl.searchParams.get('code');
-//     const next = requestUrl.searchParams.get('next') ?? '/';
-    
-//     if (!code) throw new Error('No code provided');
-//     const supabase = createRouteHandlerClient();
-//     const { error } = await supabase.auth.exchangeCodeForSession(code);
-//     if (error) throw error;
-    
-//     // URL to redirect to after sign in process completes
-//     return NextResponse.redirect(new URL(next, requestUrl));
-  
+//     if (!code) throw new Error('No code provided')
+//       const supabase = createRouteHandlerClient()
+//       const { error } = await supabase.auth.exchangeCodeForSession(code)
+//       if (error) throw error
+//       return NextResponse.redirect(redirectTo)
 //   } catch (error) {
-//     console.error(`error [${new Date().toISOString()}]:`, error);
 //     // return the user to the error page
-//     return NextResponse.redirect(new URL('/auth/error', requestUrl));
+//     const errorUrl = new URL('/auth/error', request.nextUrl)
+//     errorUrl.searchParams.set('error', `${error}`)
+//     return NextResponse.redirect(errorUrl)
 //   }
 // }
