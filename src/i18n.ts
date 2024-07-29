d7edf1adf90b0eac9ecb596d82fd3eacb8782844
loup <loup.quehec@gmail.com>
2024-07-29 12:34:14 +0200

    enable optimized image

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,13 @@
 import { getRequestConfig } from 'next-intl/server';
 import { createServerClient } from './lib/supabase/server';
+import { locales } from './lib/next-intl/navigation';
+import { notFound } from 'next/navigation';
 
 export default getRequestConfig(async ({ locale }) => {
   // if user is connected, we can get the locale from the user
   // if user is not connected, we can get the locale from the browser
   let localeToUse = locale;
+  if (!locales.includes(locale as any)) notFound();
   const supabase = createServerClient();
   const { data: { user } } = await supabase.auth.getUser();
   if (user) {
