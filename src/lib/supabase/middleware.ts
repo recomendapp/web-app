7360812ea3a3a941289a55ee3c7d406b6f292c3c
loup <loup.quehec@gmail.com>
2024-09-04 23:36:17 +0200

    update supabase

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { createServerClient } from '@supabase/ssr';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const createMiddlewareClient = ({
@@ -8,49 +8,70 @@ export const createMiddlewareClient = ({
   request: NextRequest;
   response: NextResponse;
 }) => {
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
+        getAll() {
+          return request.cookies.getAll()
         },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          });
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
           response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          });
+            request,
+          })
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options)
+          )
         },
       },
     }
-  );
+  )
+  // return createServerClient(
+  //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       get(name: string) {
+  //         return request.cookies.get(name)?.value;
+  //       },
+  //       set(name: string, value: string, options: CookieOptions) {
+  //         request.cookies.set({
+  //           name,
+  //           value,
+  //           ...options,
+  //         });
+  //         response = NextResponse.next({
+  //           request: {
+  //             headers: request.headers,
+  //           },
+  //         });
+  //         response.cookies.set({
+  //           name,
+  //           value,
+  //           ...options,
+  //         });
+  //       },
+  //       remove(name: string, options: CookieOptions) {
+  //         request.cookies.set({
+  //           name,
+  //           value: '',
+  //           ...options,
+  //         });
+  //         response = NextResponse.next({
+  //           request: {
+  //             headers: request.headers,
+  //           },
+  //         });
+  //         response.cookies.set({
+  //           name,
+  //           value: '',
+  //           ...options,
+  //         });
+  //       },
+  //     },
+  //   }
+  // );
 };
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -6,23 +6,46 @@ import {
 
 export const createServerClient = () => {
   const cookieStore = cookies();
-  return createServerClientSupabase<Database>(
+  
+  return createServerClientSupabase(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
+        getAll() {
+          return cookieStore.getAll()
         },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options });
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
     }
   );
+  // return createServerClientSupabase<Database>(
+  //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       get(name: string) {
+  //         return cookieStore.get(name)?.value;
+  //       },
+  //       set(name: string, value: string, options: CookieOptions) {
+  //         cookieStore.set({ name, value, ...options });
+  //       },
+  //       remove(name: string, options: CookieOptions) {
+  //         cookieStore.set({ name, value: '', ...options });
+  //       },
+  //     },
+  //   }
+  // );
 };
 
 export async function getSession() {
