d2f40d5346c17e736449b366d60c11f70d5e111b
loup <loup.quehec@gmail.com>
2024-09-20 17:01:39 +0200

    try fix stripe

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -24,7 +24,7 @@ const relevantEvents = new Set([
 export async function POST(request: Request) {
   const body = await request.text();
   const sig = request.headers.get('stripe-signature') as string;
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const webhookSecret = process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
   console.log(`🔔 Webhook received: sig:${sig}, body:${body}, webhookSecret:${webhookSecret}`);
   
