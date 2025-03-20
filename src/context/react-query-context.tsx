9b31a7869d76382d2daafa7e9d0425a8b0d1c688
loup <loup.quehec@gmail.com>
2025-03-20 19:24:42 +0100

    move queryClient to separate file

--- src/context/react-query-context.tsx
+++ src/context/react-query-context.tsx
@@ -1,39 +1,16 @@
 'use client';
 
-import { matchQuery, MutationCache, QueryClient, QueryClientProvider, QueryKey } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryNormalizerProvider } from '@normy/react-query';
 import { getType } from '@/lib/react-query/getType';
-// import { broadcastQueryClient } from '@tanstack/query-broadcast-client-experimental'
+import queryClient from '@/lib/react-query/queryClient';
 
 export const ReactQueryProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        staleTime: 60_000,
-      },
-    },
-    mutationCache: new MutationCache({
-      onSuccess: (_data, _variables, _context, mutation) => {
-        queryClient.invalidateQueries({
-          predicate: (query) =>
-            Array.isArray(mutation.options.meta?.invalidates) &&
-            (mutation.options.meta?.invalidates as QueryKey[]).some((queryKey) =>
-              matchQuery({ queryKey }, query),
-            ),
-        })
-      }
-    })
-  })
-  // broadcastQueryClient({
-  //   queryClient: queryClient as any,
-  //   broadcastChannel: 'recomend'
-  // })
   return (
     <QueryNormalizerProvider
       queryClient={queryClient}
---
+++ src/lib/react-query/queryClient.ts
@@ -0,0 +1,24 @@
+
+import { matchQuery, MutationCache, QueryClient, QueryKey } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 60_000,
+		},
+	},
+	mutationCache: new MutationCache({
+		onSuccess: (_data, _variables, _context, mutation) => {
+			queryClient.invalidateQueries({
+			predicate: (query) =>
+				Array.isArray(mutation.options.meta?.invalidates) &&
+				(mutation.options.meta?.invalidates as QueryKey[]).some((queryKey) =>
+				matchQuery({ queryKey }, query),
+				),
+			})
+		}
+	})
+});
+
+export default queryClient;
