d9038e26d6ff1721c6cd56fc5f173bb49ceb708d
loup <loup.quehec@gmail.com>
2025-06-01 23:40:06 +0200

    fix image share

--- src/components/ShareController/ShareControllerMedia.tsx
+++ src/components/ShareController/ShareControllerMedia.tsx
@@ -1,37 +1,58 @@
 import { ShareControllerProps } from "./ShareController";
 import { Media } from "@/types/type.db";
 import socialCanvas from "@/lib/social-canvas";
+import { useEffect, useState } from "react";
+import { Icons } from "@/config/icons";
 
 interface ShareControllerMediaProps extends ShareControllerProps {
 	media: Media;
 }
 
 export const ShareControllerMedia: React.FC<ShareControllerMediaProps> = ({ media, onFileReady }) => {
-	if (!media || !media.title || !media.avatar_url) {
-		return null;
-	}
-	const url = new URL('/v1/media/card', socialCanvas.baseUrl);
-	url.searchParams.append('title', media.title);
-	url.searchParams.append('poster', media.avatar_url);
-	if (media.main_credit && media.main_credit.length > 0) {
-		url.searchParams.append('credits', media.main_credit.map(credit => credit.title).join(', '));
-	}
-	if (media.backdrop_url) {
-		url.searchParams.append('background', media.backdrop_url);
-	}
-	return (
-		<div className="flex flex-col gap-2">
-			<img
-			alt={media.title}
-			src={url.toString()}
-			className="object-cover w-full h-64 rounded-md"
-			onLoad={(e) => {
-				const file = new File([e.currentTarget.src], `${media.title}.png`, { type: 'image/png' });
+	const [image, setImage] = useState<File | null | undefined>(undefined);
+
+	useEffect(() => {
+		const fetchImage = async () => {
+			if (!media || !media.title || !media.avatar_url) {
+				return;
+			}
+			const url = new URL('/v1/media/card', socialCanvas.baseUrl);
+			url.searchParams.append('title', media.title);
+			url.searchParams.append('poster', media.avatar_url);
+			if (media.main_credit && media.main_credit.length > 0) {
+				url.searchParams.append('credits', media.main_credit.map(credit => credit.title).join(', '));
+			}
+			if (media.backdrop_url) {
+				url.searchParams.append('background', media.backdrop_url);
+			}
+			const response = await fetch(url.toString());
+			if (response.ok) {
+				const blob = await response.blob();
+				const file = new File([blob], `${media.title}.png`, { type: 'image/png' });
+				setImage(file);
 				if (onFileReady) {
 					onFileReady(file);
 				}
-			}}
-			/>
+			}
+		}
+		fetchImage();
+	}, [media]);
+
+	return (
+		<div className="flex flex-col gap-2">
+			<div className="relative w-full h-64 flex items-center justify-center">
+				{image ? (
+					<img
+						alt={media.title ?? 'Media Image'}
+						src={URL.createObjectURL(image)}
+						className="object-cover h-full rounded-md"
+					/>
+				) : image === null ? (
+					<p className="text-muted-foreground">No image available</p>
+				) : (
+					<Icons.loader />
+				)}
+			</div>
 		</div>
 	)
 };
