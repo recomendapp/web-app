6201f031cbf956648bf7156c77d53ae8bbd596d3
loup <loup.quehec@gmail.com>
2025-06-02 12:30:08 +0200

    use POST for social-canvas

--- src/components/ShareController/ShareControllerMedia.tsx
+++ src/components/ShareController/ShareControllerMedia.tsx
@@ -3,36 +3,40 @@ import { Media } from "@/types/type.db";
 import socialCanvas from "@/lib/social-canvas";
 import { useEffect, useState } from "react";
 import { Icons } from "@/config/icons";
+import { upperFirst } from "lodash";
+import { useTranslations } from "next-intl";
 
 interface ShareControllerMediaProps extends ShareControllerProps {
 	media: Media;
 }
 
 export const ShareControllerMedia: React.FC<ShareControllerMediaProps> = ({ media, onFileReady }) => {
+	const common = useTranslations('common');
 	const [image, setImage] = useState<File | null | undefined>(undefined);
 
 	useEffect(() => {
 		const fetchImage = async () => {
-			if (!media || !media.title || !media.avatar_url) {
-				return;
-			}
-			const url = new URL('/v1/media/card', socialCanvas.baseUrl);
-			url.searchParams.append('title', media.title);
-			url.searchParams.append('poster', media.avatar_url);
-			if (media.main_credit && media.main_credit.length > 0) {
-				url.searchParams.append('credits', media.main_credit.map(credit => credit.title).join(', '));
-			}
-			if (media.backdrop_url) {
-				url.searchParams.append('background', media.backdrop_url);
-			}
-			const response = await fetch(url.toString());
+			if (!media || !media.title || !media.avatar_url) return;
+			const body = {
+				title: media.title,
+				poster: media.avatar_url,
+				credits: media.main_credit?.map(c => c.title).join(', '),
+				background: media.backdrop_url,
+			};
+
+			const response = await fetch(`${socialCanvas.baseUrl}/media/card`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			});
+
 			if (response.ok) {
 				const blob = await response.blob();
 				const file = new File([blob], `${media.title}.png`, { type: 'image/png' });
 				setImage(file);
-				if (onFileReady) {
-					onFileReady(file);
-				}
+				if (onFileReady) onFileReady(file);
 			}
 		}
 		fetchImage();
@@ -48,7 +52,7 @@ export const ShareControllerMedia: React.FC<ShareControllerMediaProps> = ({ medi
 						className="object-cover h-full rounded-md"
 					/>
 				) : image === null ? (
-					<p className="text-muted-foreground">No image available</p>
+					<p className="text-muted-foreground">{upperFirst(common('errors.an_error_occurred'))}</p>
 				) : (
 					<Icons.loader />
 				)}
