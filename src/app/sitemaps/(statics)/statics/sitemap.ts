132416757555edaecd7a3568a33bea9a194b3e23
loup <loup.quehec@gmail.com>
2025-06-04 12:07:43 +0200

    fix middleware blocking PWA

--- src/app/sitemaps/(statics)/statics/sitemap.ts
+++ src/app/sitemaps/(statics)/statics/sitemap.ts
@@ -1,4 +1,5 @@
 import { siteConfig } from '@/config/site'
+import { seoLocales } from '@/lib/i18n/routing'
 import type { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
@@ -8,24 +9,56 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
+      alternates: {
+        languages: Object.fromEntries(
+          seoLocales.map((locale) => [
+            locale,
+            `${siteConfig.url}/${locale}`
+          ])
+        )
+      }
     },
     {
       url: `${siteConfig.url}/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
+      alternates: {
+        languages: Object.fromEntries(
+          seoLocales.map((locale) => [
+            locale,
+            `${siteConfig.url}/${locale}/about`
+          ])
+        )
+      }
     },
 	{
 		url: `${siteConfig.url}/explore`,
 		lastModified: new Date(),
 		changeFrequency: 'monthly',
 		priority: 0.8,
+		alternates: {
+			languages: Object.fromEntries(
+				seoLocales.map((locale) => [
+					locale,
+					`${siteConfig.url}/${locale}/explore`
+				])
+			)
+		}
 	},
 	{
 		url: `${siteConfig.url}/upgrade`,
 		lastModified: new Date(),
 		changeFrequency: 'monthly',
 		priority: 0.8,
+		alternates: {
+			languages: Object.fromEntries(
+				seoLocales.map((locale) => [
+					locale,
+					`${siteConfig.url}/${locale}/upgrade`
+				])
+			)
+		}
 	},
   ]
 }
