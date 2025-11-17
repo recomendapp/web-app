384ebc51576cc1a85496ef649f9eab104cc8d4c4
loup <loup.quehec@gmail.com>
2025-11-17 22:00:31 +0100

    read multipart cookie for jwt verif

--- src/lib/supabase/jwt.ts
+++ src/lib/supabase/jwt.ts
@@ -5,26 +5,45 @@ import { NextRequest } from "next/server";
 
 const JWT_SECRET = new TextEncoder().encode(SUPABASE_JWT_SECRET);
 
-export const getSupabaseClaims = async (request: NextRequest): Promise<JwtPayload | null> => {
-	const raw = request.cookies.get("sb-api-auth-token")?.value;
-  	if (!raw) return null;
+export const getSupabaseClaims = async (
+  request: NextRequest
+): Promise<JwtPayload | null> => {
+  try {
+    const chunks = request.cookies
+      .getAll()
+      .filter((c) => c.name.startsWith("sb-api-auth-token"))
+      .sort(
+        (a, b) =>
+          Number(a.name.split(".")[1] || 0) -
+          Number(b.name.split(".")[1] || 0)
+      )
+      .map((c) => c.value);
 
-	try {
-		const content = raw.startsWith("base64-") ? raw.slice(7) : raw;
-		
-		const decoded = JSON.parse(Buffer.from(base64UrlToBase64(content), "base64").toString("utf8"));
+    if (!chunks.length) return null;
 
-		const jwt = decoded.access_token;
-		if (!jwt) return null;
+    const raw = chunks.join("");
+    const content = raw.startsWith("base64-") ? raw.slice(7) : raw;
+	
+    const decodedJson = Buffer.from(
+		base64UrlToBase64(content),
+		"base64"
+    ).toString("utf8");
+	
+    const decoded = JSON.parse(decodedJson);
+    const jwt = decoded.access_token;
+    if (!jwt) return null;
 
-		const { payload } = await jwtVerify(jwt, JWT_SECRET);
-		return payload as JwtPayload;
-	} catch (error) {
-		console.error('[auth] error verifying token:', error);
-		return null;
-	}
+    const { payload } = await jwtVerify(jwt, JWT_SECRET);
+    return payload as JwtPayload;
+  } catch (err) {
+    console.error("[auth] error verifying token:", err);
+    return null;
+  }
 };
 
 function base64UrlToBase64(input: string) {
-	return input.replace(/-/g, "+").replace(/_/g, "/").padEnd(Math.ceil(input.length / 4) * 4, "=");
+  return input
+    .replace(/-/g, "+")
+    .replace(/_/g, "/")
+    .padEnd(Math.ceil(input.length / 4) * 4, "=");
 }
